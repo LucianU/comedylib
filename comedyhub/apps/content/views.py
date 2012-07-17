0f32521e5e7b0e6f591098464f48f088c1aaecb4
Lucian Ursu <lucian.ursu@gmail.com>
2012-07-17 18:19:56 -0500

    Added some views to the content app

--- comedyhub/apps/content/views.py
+++ comedyhub/apps/content/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from django.views.generic import TemplateView, ListView
+
+from content.models import Collection
+
+class HomeView(TemplateView):
+    template_name = 'content/home.html'
+
+class AboutView(TemplateView):
+    template_name = 'content/about.html'
+
+class ComedianView(ListView):
+    context_object_list = 'collection_list'
+    queryset = Collection.objects.filter(role=0)
+    template_name = 'content/collection_list.html'
+
+class ShowView(ComedianView):
+    queryset = Collection.objects.filter(role=1)
+
+class MovieView(ComedianView):
+    queryset = Collection.objects.filter(role=2)
