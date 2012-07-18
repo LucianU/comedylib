d82d35b74cf54607fc71ff81ef92f7756bf6662d
Lucian Ursu <lucian.ursu@gmail.com>
2012-07-18 13:48:55 -0500

    Fixed path used to set the location of the templates directory

--- comedyhub/settings/common.py
+++ comedyhub/settings/common.py
@@ -1,8 +1,8 @@
 import os
 
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(
+SITE_ROOT = os.path.dirname(os.path.dirname(
     os.path.abspath(__file__),
-)))
+))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -110,7 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_ROOT, 'templates'),
+    os.path.join(SITE_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
