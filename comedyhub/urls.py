88ef664e0605480256d9b4e4df8fb9ebe0edfaef
Lucian Ursu <lucian.ursu@gmail.com>
2012-08-01 19:50:29 -0500

    Added admin urls

--- comedyhub/urls.py
+++ comedyhub/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls.defaults import patterns, url, include
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^', include('content.urls', namespace='content')),
+    url(r'^admin/', include(admin.site.urls)),
 )
