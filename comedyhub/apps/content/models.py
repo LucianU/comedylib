b24782ed35d75eafca2cb691a42e0ddeb37bfe64
Lucian Ursu <lucian.ursu@gmail.com>
2012-08-01 20:01:59 -0500

    Created a Profile model and put on it some relations that were on the Video model

--- comedyhub/apps/content/models.py
+++ comedyhub/apps/content/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.db import models
 
 class Collection(models.Model):
@@ -17,5 +16,3 @@ class Video(models.Model):
     title = models.CharField(max_length=255)
     url = models.URLField()
     collection = models.ForeignKey(Collection, related_name='videos')
-    fans = models.ManyToManyField(User, related_name='likes')
-    admirers = models.ManyToManyField(User, related_name='favorites')
--- comedyhub/apps/profiles/models.py
+++ comedyhub/apps/profiles/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+from content.models import Video
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    likes = models.ManyToManyField(Video, related_name='profiles')
+    favorites = models.ManyToManyField(Video, related_name='profiles')
+    lists = models.ManyToManyField(Video, related_name='profiles')
+    photo = models.ImageField()
+    description = models.TextField()
+
