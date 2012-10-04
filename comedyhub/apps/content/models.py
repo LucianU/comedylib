a59f487a6ff1a009c0147691c74094f6bb658a30
Lucian Ursu <lucian.ursu@gmail.com>
2012-10-04 11:07:13 +0300

    Made choices be tuples and added thhe 'upload_to' argument to the 'picture' field

--- comedyhub/apps/content/models.py
+++ comedyhub/apps/content/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Collection(models.Model):
     ROLE_CHOICES = (
-        '0', 'comedian',
-        '1', 'show',
-        '2', 'movie',
+        ('0', 'comedian'),
+        ('1', 'show'),
+        ('2', 'movie'),
     )
     name = models.CharField(max_length=255)
-    picture = models.ImageField()
+    picture = models.ImageField(upload_to='collections')
     description = models.TextField()
     connections = models.ManyToManyField('self', related_name='siblings')
     role = models.SmallIntegerField(choices=ROLE_CHOICES)
