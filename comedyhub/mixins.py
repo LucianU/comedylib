05d6b42636f31e1d9a37c5eb0b5df381676f957d
Lucian Ursu <lucian.ursu@gmail.com>
2012-10-15 11:40:22 +0300

    Changed the field name on the mixin to 'created'

--- comedyhub/mixins.py
+++ comedyhub/mixins.py
@@ -3,7 +3,7 @@ import datetime
 from django.db import models
 
 class CreatedMixin(models.Model):
-    added = models.DateTimeField(default=datetime.datetime.now)
+    created = models.DateTimeField(default=datetime.datetime.now)
 
     class Meta:
         abstract = True
