bba40d3e654d4ef0c82ba3b117d900a34fe1f9f5
Lucian Ursu <lucian.ursu@gmail.com>
2012-10-17 18:51:10 +0300

    Installed the captcha app

--- comedyhub/urls.py
+++ comedyhub/urls.py
@@ -6,6 +6,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^', include('content.urls', namespace='content')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^u/', include('registration.backends.default.urls')),
+    url(r'^a/', include('captcha.backends.default.urls')),
+    url(r'^a/', include('registration.backends.default.urls')),
     url(r'^u/', include('social_auth.urls')),
 )
