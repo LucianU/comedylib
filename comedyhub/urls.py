2dc8ccaba2b9f627c28330e9b5fe60e006ee92e0
Bogdan@Bogdan-PC <Bogdan@Bogdan-PC>
2012-10-18 12:45:19 +0300

    commit for updating the laptop. Modified the common and urls for the media folder

--- comedyhub/urls.py
+++ comedyhub/urls.py
@@ -1,4 +1,6 @@
+from django.conf import settings
 from django.conf.urls.defaults import patterns, url, include
+from django.conf.urls.static import static
 from django.contrib import admin
 
 admin.autodiscover()
@@ -9,3 +11,6 @@ urlpatterns = patterns('',
     url(r'^u/', include('registration.backends.default.urls')),
     url(r'^u/', include('social_auth.urls')),
 	)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
