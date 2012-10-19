b1920994b0250373e77cdfe18f99069c8a36954b
Lucian Ursu <lucian.ursu@gmail.com>
2012-10-20 01:38:40 +0300

    Fixed issue with urls

--- comedyhub/apps/content/urls.py
+++ comedyhub/apps/content/urls.py
@@ -1,15 +1,16 @@
 from django.conf.urls.defaults import patterns, url
 
 from content.views import (HomeView, AboutView, ComediansView,
-                           ShowsView, MoviesView, CollectionView)
+                           ShowsView, MoviesView, CollectionDetailView)
 
 urlpatterns = patterns('',
-    url(r'^$', HomeView.as_view()),
-    url(r'^about/$', AboutView.as_view()),
-    url(r'^comedians/', ComediansView.as_view()),
-    url(r'^comedians/(?P<pk>\d+)', CollectionView.as_view()),
-    url(r'^shows/', ShowsView.as_view()),
-    url(r'^shows/(?P<pk>\d+)', CollectionView.as_view()),
-    url(r'^movies/', MoviesView.as_view()),
-    url(r'^movies/(?P<pk>\d+)', CollectionView.as_view()),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^about/$', AboutView.as_view(), name='about'),
+    url(r'^comedians/$', ComediansView.as_view(), name='comedians'),
+    url(r'^comedians/(?P<pk>\d+)', CollectionDetailView.as_view(),
+        name='comedian'),
+    url(r'^shows/$', ShowsView.as_view(), name='shows'),
+    url(r'^shows/(?P<pk>\d+)', CollectionDetailView.as_view(), name='show'),
+    url(r'^movies/$', MoviesView.as_view(), name='movies'),
+    url(r'^movies/(?P<pk>\d+)', CollectionDetailView.as_view(), name='movie'),
 )
