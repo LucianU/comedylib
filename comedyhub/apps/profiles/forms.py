7569109c5480e7d9ad3cf560ef6495d9a22045d1
Lucian Ursu <lucian.ursu@gmail.com>
2012-10-30 12:40:39 +0200

    Using a custom login form, to be able to authenticate users with email and to have a 'remember me' checkbox

--- comedyhub/apps/profiles/forms.py
+++ comedyhub/apps/profiles/forms.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
@@ -31,3 +32,59 @@ class SignUpForm(UserCreationForm):
             user.save()
 
         return user
+
+class CustomAuthForm(forms.Form):
+    """
+    Using this custom form to perform authentication with e-mail
+    instead of username and also to add a 'remember' field
+    """
+    email = forms.CharField(initial='Your email', max_length=254)
+    password = forms.CharField(initial='Your password', label="Password",
+                               widget=forms.PasswordInput)
+    remember = forms.BooleanField(label='Keep me signed in', required=False)
+
+    error_messages = {
+        'invalid_login': "Please enter a correct username and password. "
+                         "Note that both fields are case-sensitive.",
+        'no_cookies': "Your Web browser doesn't appear to have cookies "
+                        "enabled. Cookies are required for logging in.",
+        'inactive': "This account is inactive.",
+    }
+
+    def __init__(self, request=None, *args, **kwargs):
+        """
+        If request is passed in, the form will validate that cookies are
+        enabled. Note that the request (a HttpRequest object) must have set a
+        cookie with the key TEST_COOKIE_NAME and value TEST_COOKIE_VALUE before
+        running this validation.
+        """
+        self.request = request
+        self.user_cache = None
+        super(CustomAuthForm, self).__init__(*args, **kwargs)
+
+    def clean(self):
+        email = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if email and password:
+            self.user_cache = authenticate(email=email,
+                                           password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(
+                    self.error_messages['invalid_login'])
+            elif not self.user_cache.is_active:
+                raise forms.ValidationError(self.error_messages['inactive'])
+        self.check_for_test_cookie()
+        return self.cleaned_data
+
+    def check_for_test_cookie(self):
+        if self.request and not self.request.session.test_cookie_worked():
+            raise forms.ValidationError(self.error_messages['no_cookies'])
+
+    def get_user_id(self):
+        if self.user_cache:
+            return self.user_cache.id
+        return None
+
+    def get_user(self):
+        return self.user_cache
