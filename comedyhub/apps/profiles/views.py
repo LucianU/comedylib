2ae1d21fc4fbe5c39322bbf869466dd6f624be29
Lucian Ursu <lucian.ursu@gmail.com>
2012-11-06 13:32:55 +0200

    Implemented 'home' and 'playlists' views for profiles with URLs and templates

--- comedyhub/apps/profiles/views.py
+++ comedyhub/apps/profiles/views.py
@@ -1,5 +1,27 @@
 #-*- coding: utf-8 -*-
-from django.views.generic import DetailView
+from django.shortcuts import get_object_or_404
+from django.views.generic import TemplateView
 
-class Home(DetailView):
-    pass
+from profiles.models import Profile
+
+class Home(TemplateView):
+    template_name = 'profiles/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Home, self).get_context_data(**kwargs)
+        if 'pk' in kwargs:
+            profile = get_object_or_404(Profile, user__pk=kwargs['pk'])
+            context['profile'] = profile
+        else:
+            context['profile'] = self.request.user.profile
+        return context
+
+
+class Playlists(Home):
+    template_name = 'profiles/playlists.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Playlists, self).get_context_data(**kwargs)
+        context['playlists'] = context['profile'].playlists.all()
+        return context
+
