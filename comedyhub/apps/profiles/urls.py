b43f52fe08593581f8aa019bd73cab1b592fb16e
Lucian Ursu <lucian.ursu@gmail.com>
2012-11-12 21:37:15 +0200

    Added forgotten URL for AddToPlaylist

--- comedyhub/apps/profiles/urls.py
+++ comedyhub/apps/profiles/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
-from profiles.views import Home, Playlists, VideoFeeling
+from profiles.views import Home, Playlists, VideoFeeling, AddToPlaylist
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='own_home'),
@@ -8,4 +8,5 @@ urlpatterns = patterns('',
     url(r'^(?P<pk>\w+)$', Home.as_view(), name='user_home'),
     url(r'^(?P<pk>\w+)/playlists/$', Playlists.as_view(), name='user_playlists'),
     url(r'^vfeel/$', VideoFeeling.as_view(), name='vid_feel'),
+    url(r'^addtopl/$', AddToPlaylist.as_view(), name='addtoplaylist'),
 )
