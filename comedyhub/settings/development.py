7cc73159449b514c51bb31032987d7b45ceff534
Lucian Ursu <lucian.ursu@gmail.com>
2012-11-12 21:08:49 +0200

    Added debug_toolbar to development settings

--- comedyhub/settings/development.py
+++ comedyhub/settings/development.py
@@ -1,4 +1,17 @@
 from comedyhub.settings.common import *
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+INTERNAL_IPS = (
+    '127.0.0.1',
+)
+
 try:
     from comedyhub.settings.local import *
 except ImportError:
