a250e3e18137ae9f02957049859bbf988db9a346
Lucian Ursu <lucian.ursu@gmail.com>
2012-11-26 21:18:42 +0200

    Added the Featured model to the admin.

--- comedyhub/apps/content/admin.py
+++ comedyhub/apps/content/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from content.models import Collection, Video
+from content.models import Collection, Video, Featured
 
 admin.site.register(Collection)
 admin.site.register(Video)
+admin.site.register(Featured)
