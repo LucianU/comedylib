d487c5e9a1be5c29219e14c2fc07f22337b64e37
Lucian Ursu <lucian.ursu@gmail.com>
2012-12-22 12:58:24 +0200

    Added search indexes and text templates for some models.

---
+++ comedyhub/apps/profiles/search_indexes.py
@@ -0,0 +1,21 @@
+from haystack import site
+from haystack.indexes import CharField, SearchIndex
+
+from profiles.models import Bookmark, Playlist
+
+class BookmarkIndex(SearchIndex):
+    text = CharField(document=True, use_template=True)
+
+    def index_queryset(self):
+        return Bookmark.objects.all()
+
+
+class PlaylistIndex(SearchIndex):
+    text = CharField(document=True, use_template=True)
+
+    def index_queryset(self):
+        return Playlist.objects.all()
+
+
+site.register(Bookmark, BookmarkIndex)
+site.register(Playlist, PlaylistIndex)
