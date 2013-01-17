a00ab030e93105a474ef184b1863b3f2c6e89397
Lucian Ursu <lucian.ursu@gmail.com>
2013-01-17 12:37:13 +0200

    Moved logging config to production and added raven configuration, the sentry client.

--- comedylib/settings/common.py
+++ comedylib/settings/common.py
@@ -186,29 +186,6 @@ ACCOUNT_ACTIVATION_DAYS = 3
 RECAPTCHA_PUBLIC_KEY = '6Lf-99cSAAAAAHLcXUNp14Jxhqg1AbaGvOas_uVt'
 RECAPTCHA_PRIVATE_KEY = '6Lf-99cSAAAAAHLcXUNp14Jxhqg1AbaGvOas_uVt'
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
 # Specifies the number of related videos that should be
 # displayed on the page
 RELATED_VIDS_NO = 12
