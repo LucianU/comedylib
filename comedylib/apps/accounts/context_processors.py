ac96588e2847398ec922eeba9343ec5f3f55b5f8
Lucian Ursu <lucian.ursu@gmail.com>
2013-01-28 21:26:10 +0200

    Sending form instances not classes.

--- comedylib/apps/accounts/context_processors.py
+++ comedylib/apps/accounts/context_processors.py
@@ -2,4 +2,4 @@ from accounts.forms import SignUpForm, CustomAuthForm
 
 
 def accounts_forms(request):
-    return {'login_form': CustomAuthForm, 'signup_form': SignUpForm}
+    return {'login_form': CustomAuthForm(), 'signup_form': SignUpForm()}
