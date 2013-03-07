a5bbe9d30cbb175e3d1d78e11bd51ee320f62b80
Lucian Ursu <lucian.ursu@gmail.com>
2013-03-07 17:53:13 +0200

    Redid the settings functionality to use 2 different forms.

--- comedylib/apps/profiles/forms.py
+++ comedylib/apps/profiles/forms.py
@@ -9,36 +9,5 @@ class PlaylistForm(forms.ModelForm):
         fields = ('title',)
 
 
-class SettingsForm(forms.Form):
-    error_messages = {
-        'password_mismatch': "The two password fields didn't match.",
-        'incorrect_oldpass': "The old password that you provided is incorrect.",
-    }
+class PictureForm(forms.Form):
     picture = forms.ImageField(required=False)
-    oldpassword = forms.CharField(label="Old Password",
-        widget=forms.PasswordInput)
-    password1 = forms.CharField(label="Password",
-        widget=forms.PasswordInput, required=False)
-    password2 = forms.CharField(label="Password confirmation",
-        widget=forms.PasswordInput,
-        help_text="Enter the same password as above, for verification.",
-        required=False)
-
-    def __init__(self, *args, **kwargs):
-        self.request = kwargs.pop('request')
-        super(SettingsForm, self).__init__(*args, **kwargs)
-
-    def clean_oldpassword(self):
-        oldpassword = self.cleaned_data.get('oldpassword')
-        if not self.request.user.check_password(oldpassword):
-            raise forms.ValidationError(
-                self.error_messages['incorrect_oldpass']
-            )
-
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise forms.ValidationError(
-                self.error_messages['password_mismatch'])
-        return password2
