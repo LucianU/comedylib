02ded65cdf08fcac68723736b2b08483c99d4a7f
Lucian Ursu <lucian.ursu@gmail.com>
2013-03-19 15:53:50 +0200

    Added johnny-panel to development to help with cache debugging.

--- comedylib/settings/development.py
+++ comedylib/settings/development.py
@@ -9,6 +9,7 @@ MIDDLEWARE_CLASSES += (
 
 INSTALLED_APPS += (
     'debug_toolbar',
+    'johnny_panel',
 )
 
 INTERNAL_IPS = (
@@ -19,6 +20,19 @@ DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
 }
 
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.cache.CacheDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+    'johnny_panel.panel.JohnnyPanel'
+)
 try:
     from comedylib.settings.local import *  # pylint: disable=W0614, W0401
 except ImportError:
