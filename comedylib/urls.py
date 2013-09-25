db3bcd38136c53014a0329375c87700f64c99857
Lucian Ursu <lucian.ursu@gmail.com>
2013-09-25 16:42:30 +0300

    Using the customized search view instead of the default one provided by haystack.

--- comedylib/urls.py
+++ comedylib/urls.py
@@ -6,8 +6,8 @@ from django.conf.urls.defaults import patterns, url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from haystack.views import SearchView
 from search.forms import WildcardSearchForm
+from search.views import GroupedResultsSearchView
 
 admin.autodiscover()
 
@@ -16,7 +16,7 @@ urlpatterns = patterns('',
     url(r'^a/', include('accounts.urls')),
     url(r'^u/', include('profiles.urls')),
     url(r'^c/', include('django.contrib.comments.urls')),
-    url(r'^search/', SearchView(form_class=WildcardSearchForm),
+    url(r'^search/', GroupedResultsSearchView(form_class=WildcardSearchForm),
         name='haystack_search'),
     url(r'^com/', include('affiliates.urls', namespace='affiliates')),
     url(r'^', include('content.urls', namespace='content')),
