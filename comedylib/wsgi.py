53a87b61215c0a139c53f7e106a0bf61b14d4a7d
Lucian Ursu <lucian.ursu@gmail.com>
2013-12-12 21:55:35 +0200

    Added newrelic for monitoring.

--- comedylib/wsgi.py
+++ comedylib/wsgi.py
@@ -2,4 +2,7 @@
 The entry point used by the server to run the project.
 """
 import django.core.handlers.wsgi
+import newrelic.agent
+
+newrelic.agent.initialize('../confs/production/newrelic.ini')
 application = django.core.handlers.wsgi.WSGIHandler()
