8759006a7ef644f5f4b4d8cecd652d9ddd77d52d
Lucian Ursu <lucian.ursu@gmail.com>
2013-12-12 23:30:04 +0200

    Customized Collection and Video admins.

--- comedylib/apps/content/admin.py
+++ comedylib/apps/content/admin.py
@@ -2,6 +2,23 @@ from django.contrib import admin
 
 from content.models import Collection, Video, Featured
 
-admin.site.register(Collection)
-admin.site.register(Video)
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role')
+    search_fields = ['name', 'role']
+
+    def role(self, obj):
+        return obj.get_role_display()
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'collection_name', 'created')
+    search_fields = ['title', 'collection__name']
+
+    def collection_name(self, obj):
+        return obj.collection.name
+
+
+admin.site.register(Collection, CollectionAdmin)
 admin.site.register(Featured)
+admin.site.register(Video, VideoAdmin)
