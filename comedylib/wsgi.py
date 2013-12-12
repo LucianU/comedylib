13cef9e3cc8992ccb5e37fb6e95c89c40a1e8e9d
Lucian Ursu <lucian.ursu@gmail.com>
2013-12-12 22:02:23 +0200

    Using full path to newrelic.ini.

--- comedylib/wsgi.py
+++ comedylib/wsgi.py
@@ -4,5 +4,7 @@ The entry point used by the server to run the project.
 import django.core.handlers.wsgi
 import newrelic.agent
 
-newrelic.agent.initialize('../confs/production/newrelic.ini')
+newrelic.agent.initialize(
+    '/home/comedylib/comedylib/confs/production/newrelic.ini'
+)
 application = django.core.handlers.wsgi.WSGIHandler()
