46924f73b6ec0976b57980b0d7ab3843e186afd5
Lucian Ursu <lucian.ursu@gmail.com>
2014-01-05 18:44:24 +0200

    Fixed the approach to pass the page as 'page_obj'.

--- comedylib/apps/search/views.py
+++ comedylib/apps/search/views.py
@@ -3,10 +3,9 @@ from haystack.views import SearchView
 
 class CustomSearchView(SearchView):
     """
-    Changes the name of the 'page' context object so that it can be
-    used with the pagination template.
+    Adds the 'page' context object under the name of 'page_obj' as well,
+    so that the view can use the pagination template.
     """
-    def get_context_data(self, **kwargs):
-        context = super(CustomSearchView, self).get_context_data(**kwargs)
-        context['page_obj'] = context['page']
-        return context
+    def extra_context(self):
+        (paginator, page) = self.build_page()
+        return {'page_obj': page}
