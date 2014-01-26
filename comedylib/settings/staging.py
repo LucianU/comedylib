c7fae79212e9c7bd78be3d82d294b00e263cf121
Lucian Ursu <lucian.ursu@gmail.com>
2014-01-26 17:36:02 +0200

    More adjustments to staging settings.

--- comedylib/settings/staging.py
+++ comedylib/settings/staging.py
@@ -6,7 +6,7 @@ from comedylib.settings.common import *  # pylint: disable=W0614, W0401
 DEBUG = False
 
 DATABASES['default'].update({
-    'NAME': 'stag_comedylib',
+    'NAME': 'comedylib',
     'USER': 'comedylib',
 })
 
