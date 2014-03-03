afb21fd4cc3230f851cfa824ca04390d5cc50029
Lucian Ursu <lucian.ursu@gmail.com>
2014-03-03 10:51:34 +0200

    Registered 'RelevantLink' with the admin.
    --HG--
    branch : dev

--- comedylib/apps/profiles/admin.py
+++ comedylib/apps/profiles/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from profiles.models import Profile, Playlist
+from profiles.models import Profile, Playlist, RelevantLink
 
 admin.site.register(Profile)
 admin.site.register(Playlist)
+admin.site.register(RelevantLink)
