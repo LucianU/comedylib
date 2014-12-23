b382f1cc35e676cd1920dc993c989bebcd2bf6b4
Lucian Ursu <lucian.ursu@gmail.com>
2014-12-23 06:17:20 +0200

    Fixed newrelic setup.
    --HG--
    branch : ansible-setup

--- comedylib/wsgi.py
+++ comedylib/wsgi.py
@@ -1,10 +1,12 @@
 """
 The entry point used by the server to run the project.
 """
+from django.conf import settings
 import django.core.handlers.wsgi
 import newrelic.agent
 
 newrelic.agent.initialize(
-    '/home/comedylib/comedylib/confs/production/newrelic.ini'
+    '/usr/local/etc/newrelic/comedylib/newrelic.ini',
+    settings.ENV
 )
 application = django.core.handlers.wsgi.WSGIHandler()
